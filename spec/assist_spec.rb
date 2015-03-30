041c1c1a2e3b10eff79aaf3bbeacbdb8a0a329fc
Roi Driscoll <roi.driscoll@gmail.com>
2015-03-31 00:14:51 +0100

    Fixes tests so proper stubbing occurs >_<

--- spec/assist_spec.rb
+++ spec/assist_spec.rb
@@ -7,26 +7,27 @@ describe Assist::Player do
     expect(SAMPLE_DATA[:name]).to eq("Michael Anderson")
   end
 
-    let(:player)  { Assist::Player.new           }
-    let(:profile) { double(profile: SAMPLE_DATA) }
-    let(:user)    { 'Mikuloctopus'               }
-    let(:result)  { player.profile user          }
+  let(:fake_class) { Class.new                              }
+  let(:player)     { stub_const("Assist::Player", fake_class) }
 
   describe 'GET profile' do
 
     it 'can pull in a player profile' do
-      expect(result['username']).to eq('Mikuloctopus')
+      allow(player).to receive(:profile).and_return(SAMPLE_DATA)
+      expect(player.profile[:username]).to eq('Mikuloctopus')
     end
   end
 
   describe 'Once a profile is loaded' do
 
     it 'can retrieve the name of the user' do
-      expect(result['name']).to eq('Michael Anderson')
+      allow(player).to receive(:profile).and_return(SAMPLE_DATA)
+      expect(player.profile[:name]).to eq('Michael Anderson')
     end
 
     it 'can retrive the user\'s twitter feed' do
-      expect(result['links']['twitter']).to eq('https://twitter.com/mikuloctopus')
+      allow(player).to receive(:profile).and_return(SAMPLE_DATA)
+      expect(player.profile[:links][:twitter]).to eq('https://twitter.com/mikuloctopus')
     end
   end
 
